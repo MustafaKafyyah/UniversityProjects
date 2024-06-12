cadb5a27c84310e1b4d1501b725e3b92ea423e6e
Mustafa Kafyyah <82281057+MustafaKafyyah@users.noreply.github.com>
2024-06-12 21:23:59 +0300

    app.py

--- RestaurantSearch/app.py
+++ RestaurantSearch/app.py
@@ -12,7 +12,7 @@ app.secret_key = 'key'
 db = pymysql.connect(host='localhost',
                      port=3306,
                      user='root',
-                     password='root',
+                     password='',
                      database='company',
                      cursorclass=pymysql.cursors.DictCursor)
 cursor = db.cursor()
